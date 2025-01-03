92e2c919643979729f6900c08214e2bbedf75ec5
Kaleab Asfaw Azezew <azezewka@Kaleabs-MacBook-Pro.local>
2025-01-03 16:40:11 -0600

    Revert "Added a streak counter"
    This reverts commit 31628753e4b3a0acc1e8a0b5d790227a9dd50694.

--- frontend/src/components/GroupMemberCard.tsx
+++ frontend/src/components/GroupMemberCard.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import WeeklyCalendar from './WeeklyCalendar';
-import StreakCounter from './StreakCounter';
 
 interface MemberProps {
   member: {
@@ -16,20 +15,13 @@ const GroupMemberCard: React.FC<MemberProps> = ({ member }) => {
 
   return (
     <div className="card">
-    <div className="card-header">
-      <div className="card-content">
-        <h3 className="card-title">{username}</h3>
-        <p>Total Solved: <span className="card-highlight">{totalSolved}</span></p>
-        <p>Acceptance Rate: <span className="card-highlight">{acceptanceRate.toFixed(2)}%</span></p>
-        <p>Ranking: <span className="card-highlight">{ranking}</span></p>
-      </div>
-  
-      <StreakCounter submissionCalendar={submissionCalendar} />
+      <h3 className="card-title">{username}</h3>
+      <p>Total Solved: <span className="card-highlight">{totalSolved}</span></p>
+      <p>Acceptance Rate: <span className="card-highlight">{acceptanceRate.toFixed(2)}%</span></p>
+      <p>Ranking: <span className="card-highlight">{ranking}</span></p>
+
+      <WeeklyCalendar submissionCalendar={submissionCalendar} />
     </div>
-  
-    <WeeklyCalendar submissionCalendar={submissionCalendar} />
-  </div>
-  
   );
 };
 export default GroupMemberCard;
